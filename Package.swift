87b74a11618c07a95bcd67a8b3a8bc26ed245481
Publisher <publisher@mtt.ru>
2023-11-09 10:57:58 +0300

    "updated Package.swift"

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ExolveVoiceSDK",
-            url: "https://github.com/zaitsevdm/exolve-voice-sdk/releases/download/0.0.52-dev/ExolveVoiceSDK.xcframework.zip",
-            checksum: "3b06a17306e3a17b633bd6df510a83fa8d55b3b8e0394c67433a0d2c1e6227f5"
+            url: "https://github.com/zaitsevdm/exolve-voice-sdk/releases/download/0.0.53-dev/ExolveVoiceSDK.xcframework.zip",
+            checksum: "93e7d7856f6de66096e7e129aa8fc73d317a9ee65fd995769c72337f15487c1d"
         )
     ]
 )
