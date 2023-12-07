71c7ce3238cc1c27f79a42094de7ecb9eecedec0
Publisher <publisher@mtt.ru>
2023-12-07 16:52:08 +0300

    "updated Package.swift"

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ExolveVoiceSDK",
-            url: "https://github.com/zaitsevdm/exolve-voice-sdk/releases/download/0.0.55-docs/ExolveVoiceSDK.xcframework.zip",
-            checksum: "933a026a8805c822cd3f690e67f6553d727d2362c1722327ab127fc2ee10efbe"
+            url: "https://github.com/zaitsevdm/exolve-voice-sdk/releases/download/0.0.55-test/ExolveVoiceSDK.xcframework.zip",
+            checksum: "5197904b2d1e13f49428d691b925fdbdc230bca7300d879b9b013dd662a1e409"
         )
     ]
 )
